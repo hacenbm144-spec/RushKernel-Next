fa26c1029132d437ed2e3a29003e86d13f5d2bce
Paul Lawrence <paullawrence@google.com>
2022-03-04 20:11:59 +0000

    UPSTREAM: ANDROID: fuse-bpf: Move fd operations to be synchronous
    Bug: 222619123
    Test: fuse_test passes, on cuttlefish CtsCameraTestCases passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I54c148206b5ad5ae5737939bcb076cbe6c40129c
    Signed-off-by: milxnaq <dupazlasu66@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -855,9 +855,6 @@ struct fuse_conn {
 
 	/** Protects passthrough_req */
 	spinlock_t passthrough_req_lock;
-
-	/** task_struct for fd lookups in fuse-bpf */
-	struct task_struct *task;
 };
 
 /*
@@ -1362,8 +1359,7 @@ static inline void fuse_freezer_count(void) {}
 })
 /* backing.c */
 
-struct file *fuse_fget(struct fuse_conn *fc, unsigned int fd);
-struct bpf_prog *fuse_get_bpf_prog(struct fuse_conn *fc, unsigned int fd);
+struct bpf_prog *fuse_get_bpf_prog(struct file *file);
 
 /*
  * Dummy io passed to fuse_bpf_backing when io operation needs no scratch space
